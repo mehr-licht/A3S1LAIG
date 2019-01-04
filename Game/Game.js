143cd1b917217753ea6ac211a41d63139699919e
Ricardo MG Silva <up201607780@fe.up.pt>
2019-01-04 15:15:43 +0000

    Undo and PastTabuleiros

--- Game/Game.js
+++ Game/Game.js
@@ -103,7 +103,10 @@ class Game {
         this.state = STATES.WAITING;
         //this.pieces = Array.from({ length: this.init_board.length }, (v, k) => k + 1);
         this.pieces = []; //use coords from the piece(id) object
-
+        //For undo and Film
+        this.Undo=[];
+        this.PastTabuleiros=[];
+        
         var start = Date.now();
 
 
@@ -241,6 +244,7 @@ class Game {
      * display
      */
     undo() {
+
 
     }
 
@@ -406,16 +410,19 @@ class Game {
             }
             if (this.state == STATES.READY_TO_MOVE) {
                 this.move(this.board, this.piece2Move.line, this.piece2Move.column, this.moveWhere2.line, this.moveWhere2.column, this.currentColour, this.verifyMoveReply);
-
+                let varIndexes = [this.piece2Move.line,this.piece2Move.column,this.moveWhere2.line,this.moveWhere2.column];
+                //Guarda os indexes de todas as movimentacoes
+                this.Undo.push(varIndexes);
+                
             }
 
 
             if (this.state == STATES.MOVED) {
                 this.pickedPiece = 0;
                 this.tmpPiece = 0;
-
-
-
+                //Guarda todos os tabuleiros
+                this.PastTabuleiros.push(this.board);
+               
                 this.resetError();
                 this.changeColours();
 
--- prolog/src/botsLogic.pl
+++ prolog/src/botsLogic.pl
@@ -140,34 +140,34 @@ choose_move(Tabuleiro, TabuleiroFinal,_Color, _Nivel,
     ):-
     posicoesPecasNoTabuleiro(Tabuleiro,black,ListaDePares),
     escolha(Tabuleiro, ListaDePares,ListaParaLimpar),
-    write('Lista para Limpar: '), write(ListaParaLimpar), nl,
+        %write('Lista para Limpar: '), write(ListaParaLimpar), nl,
     cleanLista(ListaParaLimpar, NovaLista), %Novalista formato [[1-1]-2,[1-3]-1,[2-2]-2,[3-3]-1,[5-1]-1]
     %escolha da peca a mover
-    write('Lista ja limpa: '), write(NovaLista), nl,   
+        %write('Lista ja limpa: '), write(NovaLista), nl,   
     length(NovaLista, SizeNovaLista),
     random(0,SizeNovaLista,IndexFuture),
-    write('Index escolhido: '), write(IndexFuture), nl,
+        %write('Index escolhido: '), write(IndexFuture), nl,
     nth0(IndexFuture, NovaLista, [LineFuture-ColumnFuture]-_),
     %fim da escolha da peca a mover
     validMoves(Tabuleiro, LineFuture, ColumnFuture, white, ListaJogadasVizinhas ),
     %escolha da direccao
     length(ListaJogadasVizinhas, SizeNew),    
     random(0,SizeNew,Index2),
-    write('ListaJogadasVizinhas: '), write(ListaJogadasVizinhas), nl,
-    write('index da jogada a fazer: '),write(Index2), nl,
+        %write('ListaJogadasVizinhas: '), write(ListaJogadasVizinhas), nl,
+        %write('index da jogada a fazer: '),write(Index2), nl,
     nth0(Index2, ListaJogadasVizinhas, [LineNova-ColumnNova]),    
   %  fim da escolha de direccao
     replaceInMatrix(Tabuleiro, LineNova, ColumnNova, black, TabuleiroNovo),
-    replaceInMatrix(TabuleiroNovo, LineFuture, ColumnFuture, empty, TabuleiroFinal),
-    write('Jogou de '),
-    write(LineFuture), 
-    write(' '),
-    write(ColumnFuture),
-    write(' para '),
-    write(LineNova),
-    write(' '),
-    write(ColumnNova),
-    write(' E acabou a jogada bot'). 
+    replaceInMatrix(TabuleiroNovo, LineFuture, ColumnFuture, empty, TabuleiroFinal).
+        %write('Jogou de '),
+        %write(LineFuture), 
+        %write(' '),
+        %write(ColumnFuture),
+        %write(' para '),
+        %write(LineNova),
+        %write(' '),
+        %write(ColumnNova),
+        %write(' E acabou a jogada bot'). 
 
 
 /**
